7e4fcaf300114078d01da711a4fa10be41a2b41c
Hannes <hannes@bitcube.co.za>
2017-12-28 21:26:53 +0200

    added the users seeds stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.count == 0
+  User.create({
+                  name: 'John',
+                  surname: 'Doe',
+                  is_active: true,
+                  username: 'john@bitcube.co.za',
+                  password: 'P@ssw0rd'
+              })
+
+  User.create({
+                  name: 'Sarah',
+                  surname: 'Doe',
+                  is_active: true,
+                  username: 'sarah@bitcube.co.za',
+                  password: 'P@ssw0rd'
+              })
+  User.create({
+                  name: 'Andrew',
+                  surname: 'Doe',
+                  is_active: true,
+                  username: 'andres@bitcube.co.za',
+                  password: 'P@ssw0rd'
+              })
+
+  User.create({
+                  name: 'Anton',
+                  surname: 'Doe',
+                  is_active: true,
+                  username: 'anton@bitcube.co.za',
+                  password: 'P@ssw0rd'
+              })
+end
