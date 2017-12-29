f0cb39343f9f490559a557a52eb9e32cb4940c13
Hannes <hannes@bitcube.co.za>
2017-12-29 16:25:32 +0200

    added bcrypt to encrypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  before_create :generate_auth_token
+
+  has_secure_password
+
   validates :name,
             :surname,
             :username,
@@ -6,4 +11,11 @@ class User < ApplicationRecord
             presence: true
 
   scope :active, -> { where(is_active: true) }
+
+  private
+  def generate_auth_token
+    begin
+      self.auth_token = SecureRandom.hex
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
