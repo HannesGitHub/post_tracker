136387fa05971cc45ecda08b8081b909f01f96ec
Hannes <hannes@bitcube.co.za>
2018-01-16 22:06:48 +0200

    asd

--- app/controllers/admin/admin_dashboard_controller.rb
+++ app/controllers/admin/admin_dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::AdminDashboardController < Admin::BaseAdminController
   def index
-    test = Figaro.env.email_server_user_name
+    puts "**************************************#{Figaro.env.email_server_user_name}***********************************"
     asdf = 3
     asdfa = 3
   end
