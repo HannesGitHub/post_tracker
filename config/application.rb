7b499e88d6e01164d66d7c84058a27c982d5bf8c
Hannes <hannes@bitcube.co.za>
2018-01-17 21:33:51 +0200

    added fonts

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module PostTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # config.autoload_paths << Rails.root.join('lib/notifier')
+    config.assets.enabled = true
     config.autoload_paths += %W(#{config.root}/app/classes)
+    config.autoload_paths += %W(#{config.root}/app/assets/fonts)
     config.autoload_paths += %W(#{config.root}/lib/modules)
     config.load_defaults 5.1
 
@@ -18,6 +20,9 @@ module PostTracker
     # See this link: http://guides.rubyonrails.org/action_mailer_basics.html
     config.action_mailer.default_url_options = { host: Figaro.env.website_domain }
     config.action_mailer.asset_host = Figaro.env.website_domain
+
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
